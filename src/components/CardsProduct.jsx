cba934c7bb052bddbcb05406a47bf25102f366b7
aditiaz <aditiaan96a@gmail.com>
2022-10-23 13:15:16 +0700

    navbar

---
+++ src/components/CardsProduct.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardsProduct = (props) => {
+  return (
+    <div className="flex ">
+      <div className="w-full h-full">
+        <img
+          src="https://dazedimg-dazedgroup.netdna-ssl.com/592/azure/dazed-prod/1060/8/1068776.jpg"
+          alt="saya"
+        />
+        <p className="text-sm font-semibold from-neutral-600 ">saya</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardsProduct;
+
+//    <div className="border w-60 h- bg-red-400">CardsPrsdoduct</div>;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './pages/App'
+import App from "./components/Layout.jsx";
+// import App from "./components/CardsProduct";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
