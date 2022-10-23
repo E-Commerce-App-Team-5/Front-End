35dd8a60762f35da90ac4889604bd7f53cb43495
aditiaz <aditiaan96a@gmail.com>
2022-10-23 14:34:20 +0700

    card for products added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily:{
-    "font-quick" : ["'Quicksand', sans-serif;"],
-  },
+    fontFamily: {
+      "font-quick": ["'Quicksand', sans-serif;"],
+    },
+
     extend: {
-      colors : {
-        "base-green" : "#122715",
-        "base" : "#69C665",
-        "light-green" : "#F8FFFA",
-        "search&text" : "#BFBFBF",
-        "text-color" : "#838383",
-        "putih" : "#FFFFFF",
-        },
+      colors: {
+        "base-green": "#122715",
+        base: "#69C665",
+        "light-green": "#F8FFFA",
+        "search&text": "#BFBFBF",
+        "text-color": "#838383",
+        putih: "#FFFFFF",
+      },
+      textColor: {
+        "text-green": "#69C665",
+        "text-grey": "#838383",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
+};
